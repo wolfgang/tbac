5e84af10e21174d1c1e008b974e73b1124a83370
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-07 14:06:57 +0100

    Tokenize all known keywords

--- src/_tests/tokenizer/tokenizer_test.rs
+++ src/_tests/tokenizer/tokenizer_test.rs
@@ -2,13 +2,26 @@ use crate::tokenizer::Token;
 use crate::tokenizer::Tokenizer;
 
 #[test]
-fn tokenizes_print_statement() {
-    let input = r#"PRINT "HELLO""#;
+fn tokenizes_print_statement_without_parameters() {
+    let input = r#"PRINT"#;
+    let result = Tokenizer::new(input).tokenize();
+
+    assert_eq!(result, Ok(vec![
+        Token::print()
+    ]))
+
+}
+
+#[test]
+fn tokenizes_all_keywords() {
+    let input = r#"PRINT IF THEN GT"#;
     let result = Tokenizer::new(input).tokenize();
 
     assert_eq!(result, Ok(vec![
         Token::print(),
-        Token::string("HELLO")
+        Token::iff(),
+        Token::then(),
+        Token::gt(),
     ]))
 
 }
--- src/tokenizer/tokenizer.rs
+++ src/tokenizer/tokenizer.rs
@@ -3,15 +3,32 @@ use crate::tokenizer::token::Token;
 type TokenizerResult = Result<Vec<Token>, String>;
 
 pub struct Tokenizer {
-
+    input_chars: Vec<char>
 }
 
 impl Tokenizer {
-    pub fn new(_input: &str) -> Self {
-        Self {}
+    pub fn new(input: &str) -> Self {
+        Self { input_chars: input.chars().collect() }
     }
 
     pub fn tokenize(&self) -> TokenizerResult {
-        Ok(Vec::new())
+        let mut position = 0;
+        let mut result = Vec::with_capacity(128);
+        while position < self.input_chars.len() {
+            if self.input_chars[position].is_uppercase() {
+                let mut buffer = String::with_capacity(16);
+                while position < self.input_chars.len() && self.input_chars[position].is_uppercase() {
+                    buffer.push(self.input_chars[position]);
+                    position += 1;
+                }
+
+                if buffer.as_str() == "PRINT" { result.push(Token::print()) }
+                if buffer.as_str() == "IF" { result.push(Token::iff())};
+                if buffer.as_str() == "THEN" { result.push(Token::then())};
+                if buffer.as_str() == "GT" { result.push(Token::gt())};
+            }
+            position += 1;
+        }
+        Ok(result)
     }
 }
