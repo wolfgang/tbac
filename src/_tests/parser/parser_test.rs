a8fca0924d22b56382743690800d52762e4a26c7
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-09 19:11:06 +0100

    Verify that a THEN can be followed by another if statement

--- src/_tests/parser/parser_test.rs
+++ src/_tests/parser/parser_test.rs
@@ -18,7 +18,7 @@ fn parse_print_node() {
 
 
 #[test]
-fn parse_if_node() {
+fn parse_if_statement() {
     let tokens = vec![
         Token::iff(),
         Token::number("1111"),
@@ -56,6 +56,38 @@ fn parse_two_statements() {
 
     assert_print_node(&node.children[0], "hello");
     assert_print_node(&node.children[1], "world");
+}
+
+#[test]
+fn parse_if_statement_with_if_statement_in_then() {
+    let tokens = vec![
+        Token::iff(),
+        Token::number("1111"),
+        Token::relop('>'),
+        Token::number("2222"),
+        Token::then(),
+        Token::iff(),
+        Token::number("3333"),
+        Token::relop('>'),
+        Token::number("4444"),
+        Token::then(),
+        Token::print(),
+        Token::string("hello"),
+    ];
+
+    let result = parse(&tokens);
+    assert!(result.is_ok());
+    let node = result.unwrap();
+    assert_eq!(node.children.len(), 1);
+
+    let if_node = as_node::<IfNode>(&node.children[0]);
+    assert_number_node(&if_node.left, 1111);
+    assert_number_node(&if_node.right, 2222);
+
+    let then_node = as_node::<IfNode>(&if_node.then);
+    assert_number_node(&then_node.left, 3333);
+    assert_number_node(&then_node.right, 4444);
+    assert_print_node(&then_node.then, "hello");
 }
 
 #[test]
