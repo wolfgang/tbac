25f612bb4e1d635c98b72595468aab8bb0ef5866
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-09 13:06:28 +0100

    Add helper function parse

--- src/_tests/parser/parser_test.rs
+++ src/_tests/parser/parser_test.rs
@@ -2,6 +2,7 @@ use crate::tokenizer::Token;
 use crate::parser::parser::Parser;
 use crate::_tests::helpers::as_node;
 use crate::parser::print_node::PrintNode;
+use crate::parser::sequence_node::SequenceNode;
 
 #[test]
 fn parse_print_node() {
@@ -10,8 +11,11 @@ fn parse_print_node() {
         Token::string("hello")
     ];
 
-    let mut parser = Parser::new(&tokens);
-    let node = parser.parse();
+    let node = parse(&tokens);
     assert_eq!(node.children.len(), 1);
     assert_eq!(as_node::<PrintNode>(&node.children[0]), &PrintNode::new("hello"))
+}
+
+fn parse(tokens: &Vec<Token>) -> SequenceNode {
+    Parser::new(tokens).parse()
 }
