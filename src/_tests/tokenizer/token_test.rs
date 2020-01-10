6c9a20ae05ba36c5b65d1be07d47c2c7faad5d32
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-10 12:24:53 +0100

    Clean up

--- src/_tests/tokenizer/token_test.rs
+++ src/_tests/tokenizer/token_test.rs
@@ -1,29 +1,20 @@
-use crate::tokenizer::token::{Token, TokenType::*};
+use crate::tokenizer::token::{Token, TokenType::*, TokenType};
 
 #[test]
 fn can_construct_tokens() {
-    let print_token = Token::print();
-    let if_token = Token::iff();
-    let then_token = Token::then();
-    let relop_gt_token = Token::relop('>');
-    let number_token = Token::number("1234");
-    let string_token = Token::string("abcd");
-    let var_token = Token::var('A');
+    assert_token(Token::print(), PRINT, "");
+    assert_token(Token::iff(), IF, "");
+    assert_token(Token::then(), THEN, "");
 
-    assert_eq!(PRINT, print_token.ttype);
-    assert_eq!(IF, if_token.ttype);
-    assert_eq!(THEN, then_token.ttype);
+    assert_token(Token::relop('>'), RELOP, ">");
 
-    assert_eq!(RELOP, relop_gt_token.ttype);
-    assert_eq!(">", relop_gt_token.value);
+    assert_token(Token::number("1234"), NUMBER, "1234");
+    assert_token(Token::string("abcd"), STRING, "abcd");
 
-    assert_eq!(NUMBER, number_token.ttype);
-    assert_eq!(number_token.value, "1234");
+    assert_token(Token::var('A'), VAR, "A");
+}
 
-    assert_eq!(STRING, string_token.ttype);
-    assert_eq!(string_token.value, "abcd");
-
-    assert_eq!(VAR, var_token.ttype);
-    assert_eq!(var_token.value, "A");
-
+fn assert_token(token: Token, ttype: TokenType, value: &str) {
+    assert_eq!(ttype, token.ttype);
+    assert_eq!(value, token.value.as_str());
 }
