2528f5e0f95f1e7ab9cb28b6b996912c2de74930
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-10 11:34:53 +0100

    Verify that compiler returns error if something goes wrong in tokenizer or parser

--- src/_tests/compiler_test.rs
+++ src/_tests/compiler_test.rs
@@ -9,7 +9,19 @@ fn compiles_if_statement() {
 
     assert_eq!(result, Ok(expected_js_code.to_string()));
 }
+
 #[test]
 fn returns_error_if_tokenizing_goes_wrong() {
+    verify_error("what",
+                 "Unrecognized character 'w'");
+}
 
+#[test]
+fn returns_error_if_parsing_goes_wrong() {
+    verify_error("THEN IF",
+                 "Expected command token but got THEN");
+}
+
+fn verify_error(code: &str, expected_error: &str) {
+    assert_eq!(compile(code), Err(expected_error.to_string()));
 }
