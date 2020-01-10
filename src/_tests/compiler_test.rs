8ce432fc34a5f29cfe30e3b10a0e11f9e3b219a6
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-10 10:12:46 +0100

    Make high-level compiler test pass

--- src/_tests/compiler_test.rs
+++ src/_tests/compiler_test.rs
@@ -1,12 +1,18 @@
-fn compile(_code: &str) -> &str {
-    return "to be implemented";
+use crate::tokenizer::Tokenizer;
+use crate::parser::parser::Parser;
+use crate::code_generator::CodeGenerator;
+
+fn compile(code: &str) -> String {
+    let tokens = Tokenizer::new(code).tokenize().unwrap();
+    let root = Parser::new(&tokens).parse().unwrap();
+    CodeGenerator {}.generate(&root)
+
 }
 
-#[ignore]
 #[test]
 fn compiles_if_statement() {
     let tb_code = r#"IF 2 > 1 THEN PRINT "YES""#;
-    let expected_js_code = r#"if (2 > 1) { console.log("YES") }"#;
+    let expected_js_code = r#"if (2 > 1) { console.log("YES"); }"#;
 
     let js_code = compile(tb_code);
 
