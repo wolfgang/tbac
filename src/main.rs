234e27d4b650586f0bc3bb9516e13dbce59e7c51
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-10 11:36:38 +0100

    Handle compiler errors

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ fn main() {
     let tb_code = r#"IF 2 > 1 THEN PRINT "YES"
                     PRINT "THE END""#;
 
-    let js_code = compile(tb_code).unwrap();
-
-    println!("{}", js_code);
+    match compile(tb_code) {
+        Ok(js_code) => { println!("{}", js_code) }
+        Err(error) => { println!("Compiler error: {}", error) }
+    }
 }
