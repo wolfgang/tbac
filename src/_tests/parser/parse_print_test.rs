4c2b7fb80a40cc598e56224a74a246e02d03f890
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-14 11:43:14 +0100

    Sketch parsing test for multiple PRINT args

--- src/_tests/parser/parse_print_test.rs
+++ src/_tests/parser/parse_print_test.rs
@@ -1,6 +1,6 @@
 use crate::tokenizer::Token;
 use crate::_tests::parser::helpers::*;
-//use crate::parser::print_node::PrintNode;
+use crate::parser::print_node::PrintNode;
 
 #[test]
 fn parses_with_single_string_parameter() {
@@ -27,21 +27,22 @@ fn parses_with_single_number_expression() {
 
 }
 
-//#[test]
-//fn parses_with_multiple_strings() {
-//    let tokens = vec![
-//        Token::print(),
-//        Token::string("hello"),
-//        Token::comma(),
-//        Token::string("world"),
-//        Token::comma(),
-//        Token::string("the end")
-//    ];
-//    let node = parse(&tokens).unwrap();
-//    assert_eq!(node.children.len(), 1);
-//    assert_is_node::<PrintNode>(&node.children[0]);
-//
-//}
+#[ignore]
+#[test]
+fn parses_with_multiple_strings() {
+    let tokens = vec![
+        Token::print(),
+        Token::string("hello"),
+        Token::comma(),
+        Token::string("world"),
+        Token::comma(),
+        Token::string("the end")
+    ];
+    let node = parse(&tokens).unwrap();
+    assert_eq!(node.children.len(), 1);
+    assert_is_node::<PrintNode>(&node.children[0]);
+
+}
 
 #[test]
 fn return_error_if_print_has_non_string_argument() {
