9a3bb5c38cd6ec237b7f799079a2089fab361b6f
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-15 13:13:03 +0100

    Clean up tests

--- src/_tests/parser/parse_expressions_test.rs
+++ src/_tests/parser/parse_expressions_test.rs
@@ -2,6 +2,7 @@ use crate::tokenizer::Token;
 use crate::_tests::parser::helpers::{parse, as_node, assert_number_node, assert_var_node};
 use crate::parser::expression_node::ExpressionNode;
 use crate::parser::let_node::LetNode;
+use crate::parser::sequence_node::SequenceNode;
 
 #[test]
 fn parses_binary_expressions_with_numbers_in_let() {
@@ -15,7 +16,6 @@ fn parses_binary_expressions_with_numbers_in_let() {
     ];
 
     let root = parse(&tokens).unwrap();
-
     assert_eq!(root.children.len(), 1);
 
     let let_node = as_node::<LetNode>(&root.children[0]);
@@ -38,17 +38,12 @@ fn parses_binary_expressions_with_left_var_in_let() {
         Token::number(10)
     ];
 
-    let root = parse(&tokens).unwrap();
+    let root = parse_single_node(&tokens);
+    let expression_node = get_let_expression(&root);
 
-    assert_eq!(root.children.len(), 1);
-
-    let let_node = as_node::<LetNode>(&root.children[0]);
-    assert_eq!(let_node.var, 'A');
-    let expression_node = as_node::<ExpressionNode>(&let_node.value);
     assert_eq!(expression_node.op, '+');
     assert_var_node(&expression_node.left, 'B');
     assert_number_node(&expression_node.right, 10);
-
 }
 
 #[test]
@@ -62,15 +57,28 @@ fn parses_binary_expressions_with_right_var_in_let() {
         Token::var('B')
     ];
 
-    let root = parse(&tokens).unwrap();
+    let root = parse_single_node(&tokens);
+    let expression_node = get_let_expression(&root);
 
-    assert_eq!(root.children.len(), 1);
-
-    let let_node = as_node::<LetNode>(&root.children[0]);
-    assert_eq!(let_node.var, 'A');
-    let expression_node = as_node::<ExpressionNode>(&let_node.value);
     assert_eq!(expression_node.op, '-');
     assert_number_node(&expression_node.left, 10);
     assert_var_node(&expression_node.right, 'B');
+}
 
+fn parse_single_node(tokens: &Vec<Token>) -> SequenceNode {
+    let root = parse(&tokens).unwrap();
+    assert_eq!(root.children.len(), 1);
+    root
+}
+
+fn get_let_expression(root: &SequenceNode) -> &ExpressionNode {
+    let let_node = verify_let_node(&root);
+    as_node::<ExpressionNode>(&let_node.value)
+
+}
+
+fn verify_let_node(root: &SequenceNode) -> &LetNode {
+    let let_node = as_node::<LetNode>(&root.children[0]);
+    assert_eq!(let_node.var, 'A');
+    let_node
 }
