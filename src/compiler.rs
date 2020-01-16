d47c4b5564e759f3937926e2e0ca06b0a9330fd2
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-16 19:15:07 +0100

    Use obscure js variable names

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,7 +1,10 @@
 use crate::code_generator::generate_code;
 
 pub fn compile(code: &str) -> Result<String, String> {
-    let runtime = "let c = 0; let r = true;function goto(n) { c = n } while (r) { switch (c) { %%CODE%% default: r = false; } }";
+    let runtime = r#"let c_ = 0; let r_ = true;
+                     function goto(n) { c_ = n }
+                     while (r_) { switch (c_) { %%CODE%% default: r_ = false; } }
+                   "#;
 
     match generate_code(code) {
         Ok(js_code) => { Ok(runtime.replace("%%CODE%%", js_code.as_str())) }
