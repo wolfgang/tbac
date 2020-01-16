cfa1586fb54833c0e8c902f1c70c7652c36c0dd8
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-16 10:59:37 +0100

    Move TokenScanner struct to module token_scanner

--- src/_tests/tokenizer/token_scanner_test.rs
+++ src/_tests/tokenizer/token_scanner_test.rs
@@ -1,69 +1,7 @@
-use regex::Regex;
 
 use crate::tokenizer::Token;
-use crate::tokenizer::token::TokenType::*;
-use crate::tokenizer::token::TokenType;
+use crate::tokenizer::token_scanner::TokenScanner;
 
-struct TokenScanner {
-    input: String,
-    index: usize,
-    token_matchers: Vec<(Regex, TokenType)>,
-}
-
-impl TokenScanner {
-    pub fn new(input: &str) -> Self {
-        Self {
-            input: input.to_string(),
-            index: 0,
-            token_matchers: vec![
-                (Regex::new("^(PRINT)").unwrap(), Print),
-                (Regex::new("^(IF)").unwrap(), If),
-                (Regex::new("^(LET)").unwrap(), Let),
-                (Regex::new("^(THEN)").unwrap(), Then),
-                (Regex::new("^([<>=])").unwrap(), RelOp),
-                (Regex::new("^([+-])").unwrap(), TermOp),
-                (Regex::new("^([*/])").unwrap(), FactOp),
-                (Regex::new("^(^[0-9]+)").unwrap(), Number),
-                (Regex::new("^(\".*\")").unwrap(), StringLiteral),
-                (Regex::new("^([A-Z])").unwrap(), Var),
-                (Regex::new("^(,)").unwrap(), Comma),
-                (Regex::new("^(\\()").unwrap(), OpenBracket),
-                (Regex::new("^(\\))").unwrap(), CloseBracket),
-            ],
-        }
-    }
-
-    pub fn next_token(&mut self) -> Result<Token, String> {
-        self.skip_whitespace();
-
-        if self.index == self.input.len() {
-            return Ok(Token::end_of_stream());
-        }
-
-        for matcher in &self.token_matchers {
-            let (regex, token_type) = matcher;
-            if regex.is_match(&self.input[self.index..]) {
-                let caps = regex.captures(&self.input[self.index..]).unwrap();
-                let captured = caps.get(1).unwrap().as_str();
-                self.index += captured.len();
-                let value = if *token_type == StringLiteral { &captured[1..captured.len() - 1] } else { captured };
-                return Ok(Token::with(*token_type, value));
-            }
-        }
-
-        return Err("Invalid token".to_string());
-    }
-
-    fn skip_whitespace(&mut self) {
-        while self.index < self.input.len() && self.current_char_is_whitespace().is_whitespace() {
-            self.index += 1
-        }
-    }
-
-    fn current_char_is_whitespace(&mut self) -> char {
-        self.input.chars().nth(self.index).unwrap()
-    }
-}
 
 #[test]
 fn scan_uppercase_tokens() {
--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -1,6 +1,6 @@
 pub use token::Token;
 pub use tokenizer::{Tokenizer, TokenizerResult, tokenize};
 
-
 pub mod token;
 pub mod tokenizer;
+pub mod token_scanner;
