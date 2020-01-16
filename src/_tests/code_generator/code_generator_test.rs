ffcf63ab58a3d27eb7b2ea89b91f5714011cc0a5
wolfgang <wolfgang@wolfgangdeutsch.work>
2020-01-16 14:21:49 +0100

    Inline former compile function in code_generator_test as 'generate'

--- src/_tests/code_generator/code_generator_test.rs
+++ src/_tests/code_generator/code_generator_test.rs
@@ -1,8 +1,11 @@
 use crate::compiler::compile;
+use crate::tokenizer::tokenize;
+use crate::parser::parser::Parser;
+use crate::code_generator::CodeGenerator;
 
 #[test]
 fn compiles_if_statement_with_expressions() {
-    verify_compilation(
+    verify_generated_code(
         r#"LET A = 10
            LET B = 20
            IF A + B > A - B THEN PRINT "YES""#,
@@ -18,26 +21,26 @@ fn compiles_if_statement_with_expressions() {
 
 #[test]
 fn compiles_print_statement_with_multiple_args() {
-    verify_compilation(r#"PRINT 1234,"hello", A"#,
-                       "console.log(1234);console.log('hello');console.log(A);\n");
+    verify_generated_code(r#"PRINT 1234,"hello", A"#,
+                          "console.log(1234);console.log('hello');console.log(A);\n");
 }
 
 #[test]
 fn compiles_print_statement_with_expressions() {
-    verify_compilation(r#"PRINT 1234 + A, B+23"#,
-                       "console.log(1234 + A);console.log(B + 23);\n");
+    verify_generated_code(r#"PRINT 1234 + A, B+23"#,
+                          "console.log(1234 + A);console.log(B + 23);\n");
 }
 
 #[test]
 fn compiles_let_with_complex_expressions() {
-    verify_compilation("LET A = 1 + 2 - 3 + B + 4",
-                       "A = 1 + 2 - 3 + B + 4;\n");
+    verify_generated_code("LET A = 1 + 2 - 3 + B + 4",
+                          "A = 1 + 2 - 3 + B + 4;\n");
 }
 
 #[test]
 fn compiles_brackets() {
-    verify_compilation("LET A = 1 * (2 + 3 * (4 + 5))",
-                       "A = 1 * (2 + 3 * (4 + 5));\n");
+    verify_generated_code("LET A = 1 * (2 + 3 * (4 + 5))",
+                          "A = 1 * (2 + 3 * (4 + 5));\n");
 }
 
 
@@ -53,11 +56,17 @@ fn returns_error_if_parsing_goes_wrong() {
                  "Expected command token but got Then");
 }
 
-fn verify_compilation(tb_code: &str, expected_js_code: &str) {
-    let result = compile(tb_code);
+fn verify_generated_code(tb_code: &str, expected_js_code: &str) {
+    let result = generate_code(tb_code);
     assert_eq!(result, Ok(expected_js_code.to_string()));
 }
 
 fn verify_error(code: &str, expected_error: &str) {
     assert_eq!(compile(code), Err(expected_error.to_string()));
+}
+
+pub fn generate_code(code: &str) -> Result<String, String> {
+    let tokens = tokenize(code)?;
+    let root = Parser::new(&tokens).parse()?;
+    Ok(CodeGenerator {}.generate(&root))
 }
--- test.js
+++ test.js
@@ -1,38 +1,35 @@
 /*
 
-10 LET C = 1
+LET C = 1
 20 PRINT "HELLO"
-30 LET C = C + 1
-40 IF C > 10 GOTO 60
-50 GOTO 20
+LET C = C + 1
+IF C > 10 GOTO 60
+GOTO 20
 60 PRINT "END"
-
-10 PRINT "HELLO"
 
  */
 
-let counter = 10;
+let counter = 0;
+let running = true;
 
 function goto(newValue) {
     counter = newValue
 }
 
-let running = true;
-
 while (running) {
     switch (counter) {
-        case 10:
+        case 0:
             C = 1;
         case 20:
             console.log(C, "\tHELLO");
-        case 30:
+        case 0:
             C = C + 1;
-        case 40:
+        case 0:
             if (C > 10) {
                 goto(60);
                 break
             }
-        case 50:
+        case 0:
             goto(20);
             break;
         case 60:
